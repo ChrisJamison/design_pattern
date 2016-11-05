ebd15641a9e1a5b254775fedcc95e0309b60fdfc
tuanvv <vutuan7.0@gmail.com>
2016-11-05 08:20:05 +0700

    tạo lại folder; updated demo và diagrams.

---
+++ Sermina/Demos/Decorator/DecoratorDemo/Food/IFood.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorDemo.Food
+{
+    public interface IFood
+    {
+        String PrepareFood();
+        double FoodPrice();  
+    }
+}
---
+++ Sermina/Demos/Decorator/DecoratorDemo/Food/VegFood.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorDemo.Food
+{
+    class VegFood: IFood
+    {
+        public String PrepareFood()
+        {
+            return "Veg Food";
+        }
+
+        public double FoodPrice()
+        {
+            return 50.0;
+        }  
+    }
+}
