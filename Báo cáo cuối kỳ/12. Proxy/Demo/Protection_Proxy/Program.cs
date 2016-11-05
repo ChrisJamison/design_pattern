82a9925828fd1fc042c02dfb27a311244f258a95
Pham Ngoc Linh <pnlinh93@gmail.com>
2016-11-05 08:38:30 +0700

    - Update structure folder - Add Proxy demo

---
+++ Báo cáo cuối kỳ/12. Proxy/Demo/Protection_Proxy/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protection_Proxy
+{
+    //Proxy Class.
+    public class ProxyImage : Image
+    {
+        RealImage realImage;
+        string url;
+
+        public ProxyImage(string url)
+        {
+            this.url = url;
+            string message = string.Format("{0}: {1}",
+                DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.fff"),
+                "Hinh anh chua duoc load");
+
+            Console.WriteLine(message);
+        }
+
+        public void ShowImage()
+        {
+            if (this.realImage == null)
+            {
+                this.realImage = new RealImage(this.url);
+            }
+            else if (this.realImage != null)
+            {
+                string message = string.Format("{0}: {1}",
+                    DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.fff"),
+                    "Hinh anh da duoc load, chi can lay ra va su dung");
+
+                Console.WriteLine(message);
+            }
+        }
+
+        public void ShowImages()
+        {
+            if (this.realImage == null)
+            {
+                this.realImage = new RealImage(this.url);
+            }
+            else if (this.realImage != null)
+            {
+                string message = string.Format("{0}: {1}",
+                    DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.fff"),
+                    "Hinh anh da duoc load, chi can lay ra va su dung");
+
+                Console.WriteLine(message);
+            }
+        }
+    }
+
+    // Real Class
+    public class RealImage : Image
+    {
+        string url;
+        
+        public RealImage(string url)
+        {
+            string message = string.Format("{0}: Hinh anh {1} vua duoc khoi tao",
+                DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.fff"), url);
+            Console.WriteLine(message);
+
+            this.url = url;
+        }
+
+        public void ShowImages()
+        {
+            string message = string.Format("{0}: Hinh anh {1} duoc load thanh cong",
+                DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.fff"), url);
+            Console.WriteLine(message);
+        }
+    }
+
+    public interface Image
+    {
+        void ShowImages();
+    }
+    
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            // Khởi tạo Proxy, Lúc này image chưa được load
+            ProxyImage image = new ProxyImage("C:\\Hinh_anh.jpg");
+
+            // Khi cần show image, lúc này hình ảnh mới thật sự được load
+            image.ShowImages();
+
+            // Khi ta sử lần tiếp theo, image không cần load lại nữa, 
+            // chỉ lấy image được cache trong proxy ra sử dụng
+            image.ShowImages();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Báo cáo cuối kỳ/12. Proxy/Demo/Virtual_Proxy/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Khởi tạo Proxy, Lúc này image chưa được load
+            ProxyImage image = new ProxyImage("C:\\Hinh_anh.jpg");
+
+            // Khi cần show image, lúc này hình ảnh mới thật sự được load
+            image.ShowImage();
+
+            // Khi ta sử lần tiếp theo, image không cần load lại nữa, 
+            // chỉ lấy image được cache trong proxy ra sử dụng
+            image.ShowImage();
+
+            Console.ReadLine();
+        }
+    }
+
+    //Proxy Class.
+    public class ProxyImage
+    {
+        RealImage realImage;
+        string url;
+
+        public ProxyImage(string url)
+        {
+            this.url = url;
+            string message = string.Format("{0}: {1}",
+                DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.fff"),
+                "Hinh anh chua duoc load");
+
+            Console.WriteLine(message);
+        }
+
+        public void ShowImage()
+        {
+            if (this.realImage == null)
+            {
+                this.realImage = new RealImage(this.url);
+            }
+            else if (this.realImage != null)
+            {
+                string message = string.Format("{0}: {1}",
+                    DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.fff"),
+                    "Hinh anh da duoc load, chi can lay ra va su dung");
+
+                Console.WriteLine(message);
+            }
+        }
+    }
+
+    // Real Class
+    public class RealImage
+    {
+        public RealImage(string url)
+        {
+            string message = string.Format("{0}: Hinh anh {1} bat dau duoc load",
+                DateTime.Now.ToString("yyyy/MM/dd hh:mm:ss.fff"), url);
+            Console.WriteLine(message);
+        }
+    }
+}
