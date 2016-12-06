43cc22c013b656ff9b3a65eca33fdfa7c97e2217
Pham Ngoc Linh <pnlinh93@gmail.com>
2016-12-06 23:06:00 +0700

    Complete Singleton

---
+++ Báo cáo cuối kỳ/5. Singleton/Demo/Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Table.Instance.Show();
+
+            // Ta không thể tạo một thể hiện mới như thông thường
+            // Tất cả phải sử dụng thông qua một thể hiện Instance được cung cấp từ lớp Singleton
+            // Table table = new Table();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Báo cáo cuối kỳ/5. Singleton/Demo/Singleton/Table.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Table
+    {
+        public static Table Instance = new Table();
+        private Table() { }
+        public void Show()
+        {
+            Console.WriteLine("Hi there!");
+        }
+    }
+}
