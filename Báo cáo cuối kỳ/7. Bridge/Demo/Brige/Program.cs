0fd9ae9185a7e7abe17aa7d9a3d4bb4509257934
Pham Ngoc Linh <pnlinh93@gmail.com>
2016-12-07 23:59:48 +0700

    Beta version - Bridge Pattern

---
+++ Báo cáo cuối kỳ/7. Bridge/Demo/Brige/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Brige
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            HienThi _hienThiWin = new Win();
+            _hienThiWin.Ve = new VeHcn();
+            _hienThiWin.Show();
+
+            HienThi _hienThiLinux = new Linux();
+            _hienThiLinux.Ve = new VeHcn();
+            _hienThiLinux.Show();
+            Console.Read();
+
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Báo cáo cuối kỳ/7. Bridge/Demo/Brige/VeHcn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Brige
+{
+    // "ConcreteImplementorA"
+    class VeHcn : Ve
+    {
+        //ve hinh chu nhat tren win
+        public override void HcnWin()
+        {
+            MessageBox.Show("Ve hcn tren Win");
+        }
+        //ve hinh chu nhat tren linux
+        public override void HcnLinux()
+        {
+            MessageBox.Show("Ve hcn tren Linux");
+        }
+    }
+}
