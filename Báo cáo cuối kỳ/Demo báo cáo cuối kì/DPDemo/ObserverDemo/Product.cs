0d05b0609ed0b9daaebc61b62b5168ec912dc363
Tuấn Vũ Văn <vutuan7.0@gmail.com>
2017-01-06 23:45:09 +0700

    updated báo cáo + full demo

---
+++ Báo cáo cuối kỳ/Demo báo cáo cuối kì/DPDemo/ObserverDemo/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverDemo
+{
+    class Product : Observer
+    {
+        private string name;
+
+        List<Customer> customers = new List<Customer>();        
+
+        public Product(string name)
+        {
+            this.name = name;
+        }
+
+        public override void ObserverInfo()
+        {
+            foreach (Customer item in customers)
+            {
+                if (item != null)
+                {
+                    item.ObserverInfo();
+                }
+            }
+        }
+
+        public void AddCustomer(Customer cus) {
+            if (cus != null)
+            {
+                this.customers.Add(cus);
+            }
+        }
+
+        public void RemoveCustomer(Customer cus)
+        {
+            if (cus != null)
+            {
+                this.customers.Remove(cus);
+            }
+        }
+    }
+}
---
+++ Báo cáo cuối kỳ/Demo báo cáo cuối kì/DPDemo/ObserverDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverDemo
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product("Laptop Toshiba 14 inch");
+
+            Customer linh = new Customer("Linh");
+            Customer khoa = new Customer("Khoa");
+
+            product.AddCustomer(linh);
+            product.AddCustomer(khoa);
+
+            product.ObserverInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
